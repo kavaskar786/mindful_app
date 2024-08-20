bfe763e5ad2520cde77091a3477647fb1505f620
likithayadav_g <minutommmy1919@gmail.com>
2024-08-20 10:01:45 +0530

    just changed

--- src/components/NavigationBar/StyledComponent/index.js
+++ src/components/NavigationBar/StyledComponent/index.js
@@ -24,7 +24,7 @@ export const NavigationBarMenu = ({ isOpenMenu, openDrawer }) => (
 // Component for navigation bar title
 export const NavigationBarTitle = () => (
   <NavLink to="/">
-    <Typography variant="h5">MindfulMe </Typography>
+    <Typography variant="h5">MindCare </Typography>
   </NavLink>
 );
 
